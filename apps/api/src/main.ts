6ef75dba69d130fe478532a0eaa07b588f059a93
Michael Angelo <55844504+michaelangrivera@users.noreply.github.com>
2022-04-09 17:25:01 -0400

    init commit

---
+++ apps/api/src/main.ts
@@ -0,0 +1,26 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+
+import { AppModule } from './app/app.module'
+
+import { google } from 'googleapis'
+
+async function bootstrap() {
+  const test = google._options
+  console.log(test.baseURL)
+  const app = await NestFactory.create(AppModule)
+  const globalPrefix = 'api'
+  app.setGlobalPrefix(globalPrefix)
+  const port = process.env.PORT || 3333
+  await app.listen(port)
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  )
+}
+
+bootstrap()
