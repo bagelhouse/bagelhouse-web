e6b9b265cd3d29053bdcde203f5ab2ef001dba62
Michael Angelo <55844504+michaelangrivera@users.noreply.github.com>
2022-05-09 09:34:22 -0400

    revamp testing environment

--- apps/gdrive-s3-sync-api/src/app/app.service.ts
+++ apps/gdrive-s3-sync-api/src/app/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to my-nest-app!' }
+  getData(): { data: string } {
+    return { data: 'Welcome to my-nest-app!' }
   }
 }
--- e2e/cypress/integration/demoapp/demo.spec.ts
+++
@@ -1,9 +0,0 @@
-import '../../support';
-
-describe('Base test', () => {
-  it('should have the correct title', () => {
-    cy.visit('/')
-      .get('h1')
-      .should('contain', 'Welcome to demoapp');
-  });
-});
